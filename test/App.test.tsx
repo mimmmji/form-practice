dc6864b8c3268e088fd05119b411403712d08724
Junsuk Park <park64kr63@gmail.com>
2023-03-28 11:25:44 +0900

    0328 session

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -23,7 +23,6 @@ describe('SimpleForm', () => {
         expect(input).toBeInTheDocument();
     })
 
-
     it('should validate length of TextField', () => {
         const { getByTestId, getByLabelText } = render(
             <SimpleForm>
@@ -35,32 +34,32 @@ describe('SimpleForm', () => {
                 />
             </SimpleForm>
         );
-        
+
         fireEvent.change(getByLabelText('name'), { target: { value: '1234' } });
-        
+
         const errorElement = getByTestId('error-message');
         expect(errorElement).toBeInTheDocument();
     })
-    
+
 
     it('should submit if there are no errors', () => {
         const inputValues = { name: 'minji', password: '12345' };
-      
+
         const { getByLabelText, getByText, getByTestId } = render(
           <SimpleForm>
             <TextField id={'name'} source={'name'} label={'이름'} validate={[min(5), max(10), required()]} />
             <TextField id={'password'} source={'password'} label={'비밀번호'} validate={[min(5), max(10), required()]} />
           </SimpleForm>
         );
-        
+
         fireEvent.change(getByLabelText('이름'), { target: { value: inputValues.name } });
         fireEvent.change(getByLabelText('비밀번호'), { target: { value: inputValues.password } });
-        
+
         fireEvent.submit(getByText('제출'));
-      
+
         expect(() => getByTestId('error-message')).toThrow();
         expect(getByLabelText('이름')).toHaveValue(inputValues.name);
         expect(getByLabelText('비밀번호')).toHaveValue(inputValues.password);
       });
-    
+
 });
---
+++ test/CheckboxField.test.tsx
@@ -0,0 +1,38 @@
+import {fireEvent, render, screen} from "@testing-library/react";
+import SimpleForm from "../src/components/SimpleForm";
+import CheckboxField from "../src/components/CheckboxField";
+
+describe('CheckboxField', () => {
+  it('should render CheckboxField', () => {
+      render(
+            <SimpleForm>
+                <CheckboxField
+                    id={'smoking'}
+                    source={'smoking'}
+                    label={'흡연 여부'}
+                />
+            </SimpleForm>
+      );
+
+      expect(screen.getByLabelText('흡연 여부')).toBeInTheDocument();
+  });
+
+    it('should render CheckboxField', () => {
+        render(
+            <SimpleForm>
+                <CheckboxField
+                    id={'smoking'}
+                    source={'smoking'}
+                    label={'흡연 여부'}
+                />
+            </SimpleForm>
+        );
+
+        const checkboxElement = screen.getByLabelText('흡연 여부');
+
+        fireEvent.click(checkboxElement);
+
+        // controlled component vs uncontrolled component
+        expect(checkboxElement).toBeChecked();
+    });
+});
